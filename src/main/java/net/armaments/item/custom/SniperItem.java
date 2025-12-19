914d1455729b00f3e3783653d4c5ff10c1a7cc85
Evan2Awesome <139525977+Evan2Awesome@users.noreply.github.com>
2025-12-18 21:07:25 -0700

    I met the sniperer

--- src/main/java/net/armaments/item/custom/SniperItem.java
+++ src/main/java/net/armaments/item/custom/SniperItem.java
@@ -1,5 +1,10 @@
 package net.armaments.item.custom;
 
+import net.armaments.client.ModSounds;
+import net.armaments.entity.ModDamageSources;
+import net.armaments.item.component.ModDataComponents;
+import net.armaments.util.Functions;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
@@ -31,5 +36,14 @@ public class SniperItem extends AbstractGunItem implements GunItem {
     }
 
     @Override
-    public void shoot(PlayerEntity shooter, ItemStack gun) {}
+    public void shoot(PlayerEntity shooter, ItemStack gun) {
+        if (this.getAmmo(gun) >= 1) {
+            gun.damage(1, shooter, gun.equals(shooter.getMainHandStack()) ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND);
+            gun.set(ModDataComponents.AMMO, this.getAmmo(gun) - 1);
+            shooter.playSound(ModSounds.GUNSHOT);
+            if (Functions.raycastEntity(shooter, 100d) instanceof LivingEntity entity) entity.damage(ModDamageSources.of(shooter).revolver(shooter), this.getDamage(gun, shooter));
+            shooter.setPitch(shooter.getPitch() - shooter.getRandom().nextBetweenExclusive(1, 11));
+            shooter.setYaw(shooter.getYaw() - shooter.getRandom().nextBetweenExclusive(-2, 3));
+        }
+    }
 }
